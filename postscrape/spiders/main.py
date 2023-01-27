3f23fe61c4f1054a1840341a322fdfd8bbb620b9
Whaley42 <jake.whaley1@gmail.com>
2023-01-26 21:30:06 -0600

    Delete postscrape directory

--- postscrape/spiders/main.py
+++
@@ -1,7 +0,0 @@
-from Classes.ufc_spider import UfcSpider
-from scrapy.crawler import CrawlerProcess
-
-if __name__ == "__main__":
-  process = CrawlerProcess()
-  process.crawl(UfcSpider)
-  process.start()
